57af7f17def3839b8af738cc4993287d26d33a19
Nuthan Prasad <nuthan1159@gmail.com>
2022-10-12 09:09:08 +0530

    remove api keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,12 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.17",
   etherscan: {
-    apiKey: "B9SEZ4K5MAMTI9FEPDQRBIQP9GW4WN5GFG",
+    apiKey: "",
   },
   networks : {
     goerli : {
-      url : "https://morning-proud-log.ethereum-goerli.discover.quiknode.pro/54ba1889078030f0d9e7e11cb49481298b031637/",
-      accounts : ["059bcd5d85e0f99074148e694668d244d90b98bc50bc1c8d4f26785741a5e4c9"],
+      url : "",
+      accounts : [""],
     }
   }
 };
